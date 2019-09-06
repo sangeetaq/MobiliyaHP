e521a78d483c2605bb81162c63f628b9fc69e16d
Sangeeta Khare <Sangeeta Khare@DESKTOP-PES8REG>
2019-09-06 12:37:44 +0530

    Demo Application
    Change-Id: I0000000000000000000000000000000000000000
    Signed-off-by: Sangeeta Khare <Sangeeta Khare@DESKTOP-PES8REG>

---
+++ src/main/java/com/mobiliya/controller/EmployeeController.java
@@ -0,0 +1,68 @@
+package com.mobiliya.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mobiliya.bean.Employee;
+import com.mobiliya.service.IEmployeeService;
+
+@RestController
+public class EmployeeController {
+
+	@Autowired
+	IEmployeeService employeeService;
+
+	@GetMapping("/employee/{empId}")
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public ResponseEntity<Employee> getEmployeeDetails(@PathVariable("empId") int id) {
+		Employee employee = employeeService.getEmployee(id);
+		ResponseEntity<Employee> response = new ResponseEntity<Employee>(employee, HttpStatus.OK);
+		return response;
+
+	}
+
+	@GetMapping("/employee")
+	@PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
+	public ResponseEntity<List<Employee>> getEmployees() {
+		List<Employee> employee = employeeService.getEmployees();
+		ResponseEntity<List<Employee>> response = new ResponseEntity<List<Employee>>(employee, HttpStatus.CREATED);
+		return response;
+	}
+
+	@PostMapping(value = "/employee")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
+		employeeService.addEmployee(employee);
+		ResponseEntity<Employee> response = new ResponseEntity<Employee>(employee, HttpStatus.CREATED);
+		return response;
+	}
+
+	@PutMapping(value = "/employee")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
+		employeeService.addEmployee(employee);
+		ResponseEntity<Employee> response = new ResponseEntity<Employee>(employee, HttpStatus.CREATED);
+		return response;
+	}
+
+	@DeleteMapping("/employee/{empId}")
+	@PreAuthorize("hasRole('ADMIN')")
+	public ResponseEntity<Employee> deleteEmployee(@PathVariable("empId") int id) {
+		employeeService.deleteEmployee(id);
+		ResponseEntity<Employee> response = new ResponseEntity<Employee>(HttpStatus.OK);
+		return response;
+	}
+
+}
---
+++ src/test/java/com/mobiliya/main/EmployeeControllerTest.java
@@ -0,0 +1,115 @@
+package com.mobiliya.main;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mobiliya.bean.Employee;
+import com.mobiliya.controller.AuthRestAPIs;
+import com.mobiliya.controller.EmployeeController;
+import com.mobiliya.util.JwtRequest;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EmployeeControllerTest {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@InjectMocks
+	EmployeeController employeeController;
+
+	@Autowired
+	AuthRestAPIs jwtAuthenticationController;
+
+	@Test
+	public void getEmployees() throws Exception {
+		JwtRequest request = new JwtRequest();
+		request.setUsername("adamgkz");
+		request.setPassword("123456789");
+		String accessToken = jwtAuthenticationController.createAuthenticationToken(request);
+		mockMvc.perform(MockMvcRequestBuilders.get("/employee").header("Authorization", "Bearer " + accessToken))
+				.andExpect(MockMvcResultMatchers.status().isCreated()).andDo(MockMvcResultHandlers.print());
+	}
+
+	@Test
+	public void getEmployeeDetails() throws Exception {
+		JwtRequest request = new JwtRequest();
+		request.setUsername("adamgkz");
+		request.setPassword("123456789");
+
+		String accessToken = jwtAuthenticationController.createAuthenticationToken(request);
+		mockMvc.perform(
+				MockMvcRequestBuilders.get("/employee/{empId}", 102).header("Authorization", "Bearer " + accessToken))
+				.andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.empId").value(102));
+	}
+
+	@Test
+	public void addEmployee() throws Exception {
+		JwtRequest request = new JwtRequest();
+		request.setUsername("thomasgkz");
+		request.setPassword("123456789");
+
+		String accessToken = jwtAuthenticationController.createAuthenticationToken(request);
+		Employee employee = new Employee();
+		employee.setEmpId(101);
+		employee.setEmpName("Sangeeta");
+
+		MvcResult mvcResult = mockMvc
+				.perform(MockMvcRequestBuilders.post("/employee").header("Authorization", "Bearer " + accessToken)
+						.content(new ObjectMapper().writeValueAsString(employee))
+						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andReturn();
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(201, status);
+		String content = mvcResult.getResponse().getContentAsString();
+	}
+
+	@Test
+	public void updateEmployee() throws Exception {
+		JwtRequest request = new JwtRequest();
+		request.setUsername("thomasgkz");
+		request.setPassword("123456789");
+
+		String accessToken = jwtAuthenticationController.createAuthenticationToken(request);
+		Employee employee = new Employee();
+		employee.setEmpId(101);
+		employee.setEmpName("Sangeeta");
+		MvcResult mvcResult = mockMvc
+				.perform(MockMvcRequestBuilders.put("/employee").header("Authorization", "Bearer " + accessToken)
+						.content(new ObjectMapper().writeValueAsString(employee))
+						.contentType(MediaType.APPLICATION_JSON).accept(MediaType.APPLICATION_JSON))
+				.andReturn();
+		int status = mvcResult.getResponse().getStatus();
+		assertEquals(201, status);
+		String content = mvcResult.getResponse().getContentAsString();
+	}
+
+	@Test
+	public void deleteEmployee() throws Exception {
+		JwtRequest request = new JwtRequest();
+		request.setUsername("thomasgkz");
+		request.setPassword("123456789");
+
+		String accessToken = jwtAuthenticationController.createAuthenticationToken(request);
+		mockMvc.perform(MockMvcRequestBuilders.delete("/employee/101").header("Authorization", "Bearer " + accessToken)
+				.accept(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk());
+	}
+
+}
